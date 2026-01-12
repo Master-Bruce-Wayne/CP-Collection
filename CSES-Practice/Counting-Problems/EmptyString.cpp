17c5c57aace90bff51327ea191f80b02f1988579
Sujal Agarwal <sujalagarwal.nitdgp@gmail.com>
2026-01-12 16:59:28 +0530

    Counting Problems Section Created

---
+++ CSES-Practice/Counting-Problems/EmptyString.cpp
@@ -0,0 +1,112 @@
+// counting -> empty strings
+
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define ll long long
+#define ld long double
+#define PB push_back
+#define MP make_pair
+#define EB emplace_back
+#define ff first
+#define ss second
+#define all(x) x.begin(), x.end()
+#define clr(x) memset(x, 0, sizeof(x))
+#define setval(x, y) memset(x, y, sizeof(x))
+#define sortall(x) sort(all(x))
+#define revsortall(x) sort(x.rbegin(), x.rend())
+#define lbnd        lower_bound
+#define ubnd        upper_bound
+#define remin(a, b) (a = min((a), (b)))
+#define remax(a, b) (a = max((a), (b)))
+#define sz(x) (ll)(x).size()
+#define PI 3.1415926535897932384626
+#define INF 1e18
+#define endl '\n'
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+ 
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<pii> vpii;
+typedef vector<pl> vpl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+typedef vector<string> vstr;
+ 
+#define whatis(x)   cout << #x << " is " << x << "\n";
+#define debug(x)     cout << #x << ": " << x << endl;
+#define debugTwo(x,y)   cout << #x << ": " << x << ", " << #y << ": " << y << endl; 
+#define debugThree(x,y,z)  cout << #x << ": " << x << ", " << #y << ": " << y << ", " << #z << ": " << z << endl; 
+#define debug_p(x)   cout << #x << ": " << x.F << " " << x.S << endl;
+#define debug_v(x)   {cout << #x << ": "; for (auto ioi : x) cout << ioi << " "; cout << endl;}
+#define debug_vp(x)  {cout << #x << ": "; for (auto ioi : x) cout << '[' << ioi.F << " " << ioi.S << ']'; cout << endl;}
+#define debug_v_v(x) {cout << #x << "/*\n"; for (auto ioi : x) { for (auto ioi2 : ioi) cout << ioi2 << " "; cout << '\n';} cout << "*/" << #x << endl;}
+ 
+const ll MOD = 1e9 + 7;
+// ************* Code Starts ****************** //
+ 
+ 
+// *** Sujal Agarwal *** // 
+// *** NIT DGP'28  *** // 
+ 
+// Important Points ->
+// 1. upper_bound points to first value > val
+// 2. lower_bound me equal to bhi aata hai (>=)
+
+
+ll choose[260][260];
+void ncr_compute() {
+    for(ll i=0; i<260; i++) {
+        choose[i][0]=1;
+        for(ll j=1; j<=i; j++) {
+            choose[i][j] = (choose[i-1][j]+choose[i-1][j-1])%MOD;
+        }
+    }
+}
+
+void solve() {
+    // ll n; cin>>n;
+    string s; cin>>s;
+    ll n=s.length();
+    vvl dp(n+1, vl(n+1,0));
+    for(ll i=0; i<n; i++)  dp[i+1][i]=1;
+
+    for(ll i=n-1; i>=0; i--) {
+        for(ll j=i+1; j<n; j+=2) {
+            for(ll k=i+1; k<=j; k+=2) {
+                if(s[i]==s[k]) {
+                    ll temp=(1LL* dp[i+1][k-1]* dp[k+1][j])%MOD;
+                    ll temp2 = (1LL* temp* choose[(j-i+1)/2][(k-i+1)/2])%MOD;
+                    dp[i][j]=(dp[i][j]+temp2)%MOD;
+                }
+            }
+        }
+    }
+
+    cout<<dp[0][n-1];
+}
+ 
+ 
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    ll tc = 1;
+    // cin >> tc;
+    while(tc--){
+        ncr_compute();
+        solve();
+        // cout << endl;
+    } 
+ 
+    // for(int i=1;i<=tc;i++)
+    // {
+    //   cout<<"Case #"<<i<<": ";
+    //   solve();
+    // }
+ 
+    return 0;
+}
