00e2569a42a9690ee413c74f5c777979046cb3b3
Sujal Agarwal <sujalagarwal.nitdgp@gmail.com>
2026-01-12 16:47:58 +0530

    Sliding Window Section Added

---
+++ CSES-Practice/Sliding-Window/SlidingWindowCost.cpp
@@ -0,0 +1,130 @@
+// sliding window -> sliding window cost
+
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define ll long long
+#define ld long double
+#define PB push_back
+#define MP make_pair
+#define EB emplace_back
+#define ff first
+#define ss second
+#define all(x) x.begin(), x.end()
+#define clr(x) memset(x, 0, sizeof(x))
+#define setval(x, y) memset(x, y, sizeof(x))
+#define sortall(x) sort(all(x))
+#define revsortall(x) sort(x.rbegin(), x.rend())
+#define lbnd        lower_bound
+#define ubnd        upper_bound
+#define remin(a, b) (a = min((a), (b)))
+#define remax(a, b) (a = max((a), (b)))
+#define sz(x) (ll)(x).size()
+#define PI 3.1415926535897932384626
+#define INF 1e18
+#define endl '\n'
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+ 
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<pii> vpii;
+typedef vector<pl> vpl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+typedef vector<string> vstr;
+ 
+#define whatis(x)   cout << #x << " is " << x << "\n";
+#define debug(x)     cout << #x << ": " << x << endl;
+#define debugTwo(x,y)   cout << #x << ": " << x << ", " << #y << ": " << y << endl; 
+#define debugThree(x,y,z)  cout << #x << ": " << x << ", " << #y << ": " << y << ", " << #z << ": " << z << endl; 
+#define debug_p(x)   cout << #x << ": " << x.F << " " << x.S << endl;
+#define debug_v(x)   {cout << #x << ": "; for (auto ioi : x) cout << ioi << " "; cout << endl;}
+#define debug_vp(x)  {cout << #x << ": "; for (auto ioi : x) cout << '[' << ioi.F << " " << ioi.S << ']'; cout << endl;}
+#define debug_v_v(x) {cout << #x << "/*\n"; for (auto ioi : x) { for (auto ioi2 : ioi) cout << ioi2 << " "; cout << '\n';} cout << "*/" << #x << endl;}
+ 
+const ll MOD = 1e9 + 7;
+// ************* Code Starts ****************** //
+ 
+ 
+// *** Sujal Agarwal *** // 
+// *** NIT DGP'28  *** // 
+ 
+// Important Points ->
+// 1. upper_bound points to first value > val
+// 2. lower_bound me equal to bhi aata hai (>=)
+
+ll n,k;
+multiset<ll> up, low;
+ll slo=0,sup=0;
+
+void insert(ll val) {
+    if(up.empty() || val<(*up.begin())) {
+        low.insert(val); slo+=val;
+
+        if(low.size()>(k+1)/2) {
+            ll ele=*(low.rbegin());
+            slo-=ele; sup+=ele;
+            low.erase(low.find(ele)); up.insert(ele);
+        }
+    }
+    else {
+        up.insert(val); sup+=val;
+        if(up.size()>(k/2)) {
+            ll ele=*(up.begin());
+            slo+=ele; sup-=ele;
+            low.insert(ele); up.erase(up.find(ele));
+        }
+    }
+}
+
+ll costfunc(ll med) {
+    ll sz1=low.size(), sz2=up.size(); 
+    ll p1=(med*sz1)-slo;
+    ll p2=sup-(med*sz2);
+    return p1+p2;
+}
+
+void solve() {
+    cin>>n>>k;
+    vl a(n);
+    for(ll i=0; i<n; i++)  cin>>a[i];
+
+    for(ll i=0; i<=k-2; i++) {  insert(a[i]); }
+
+    for(ll i=k-1; i<n; i++) {
+        insert(a[i]);
+        ll cf2=INF;
+        ll cf1=costfunc(*(low.rbegin()));
+        if(up.size()>0) cf2=costfunc(*(up.begin()));
+        cout<<min(cf1,cf2)<<" ";
+
+        if(a[i-k+1]<=*(low.rbegin())) {
+            low.erase(low.find(a[i-k+1]));  slo-=a[i-k+1];
+        }
+        else { up.erase(up.find(a[i-k+1])); sup-=a[i-k+1]; }
+    }
+
+} 
+ 
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    ll tc = 1;
+    // cin >> tc;
+    while(tc--){
+        solve();
+        // cout << endl;
+    } 
+ 
+    // for(int i=1;i<=tc;i++)
+    // {
+    //   cout<<"Case #"<<i<<": ";
+    //   solve();
+    // }
+ 
+    return 0;
+}
---
+++ CSES-Practice/Sliding-Window/SlidingWindowMedian.cpp
@@ -0,0 +1,97 @@
+// sliding window -> sliding window median
+
+#include <bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+template <typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+ 
+#define ll long long
+#define ld long double
+#define PB push_back
+#define MP make_pair
+#define EB emplace_back
+#define ff first
+#define ss second
+#define all(x) x.begin(), x.end()
+#define clr(x) memset(x, 0, sizeof(x))
+#define setval(x, y) memset(x, y, sizeof(x))
+#define sortall(x) sort(all(x))
+#define revsortall(x) sort(x.rbegin(), x.rend())
+#define lbnd        lower_bound
+#define ubnd        upper_bound
+#define remin(a, b) (a = min((a), (b)))
+#define remax(a, b) (a = max((a), (b)))
+#define sz(x) (ll)(x).size()
+#define PI 3.1415926535897932384626
+#define INF 1e18
+#define endl '\n'
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+ 
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<pii> vpii;
+typedef vector<pl> vpl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+typedef vector<string> vstr;
+ 
+#define whatis(x)   cout << #x << " is " << x << "\n";
+#define debug(x)     cout << #x << ": " << x << endl;
+#define debugTwo(x,y)   cout << #x << ": " << x << ", " << #y << ": " << y << endl; 
+#define debugThree(x,y,z)  cout << #x << ": " << x << ", " << #y << ": " << y << ", " << #z << ": " << z << endl; 
+#define debug_p(x)   cout << #x << ": " << x.F << " " << x.S << endl;
+#define debug_v(x)   {cout << #x << ": "; for (auto ioi : x) cout << ioi << " "; cout << endl;}
+#define debug_vp(x)  {cout << #x << ": "; for (auto ioi : x) cout << '[' << ioi.F << " " << ioi.S << ']'; cout << endl;}
+#define debug_v_v(x) {cout << #x << "/*\n"; for (auto ioi : x) { for (auto ioi2 : ioi) cout << ioi2 << " "; cout << '\n';} cout << "*/" << #x << endl;}
+ 
+const ll MOD = 1e9 + 7;
+// ************* Code Starts ****************** //
+ 
+ 
+// *** Sujal Agarwal *** // 
+// *** NIT DGP'28  *** // 
+ 
+// Important Points ->
+// 1. upper_bound points to first value > val
+// 2. lower_bound me equal to bhi aata hai (>=)
+
+
+void solve() {
+    ll n,k; cin>>n>>k;
+    vl a(n);
+    for(ll i=0; i<n; i++)  cin>>a[i];
+
+    ordered_set<pl> st;
+    for(ll i=0; i<k-1; i++)  st.insert({a[i],i});
+    for(ll i=k-1; i<n; i++) {
+        st.insert({a[i],i});
+        cout<< (*(st.find_by_order((k-1)/2))).ff << " ";
+        st.erase({a[i-k+1],i-k+1});
+    }
+} 
+ 
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    ll tc = 1;
+    // cin >> tc;
+    while(tc--){
+        solve();
+        // cout << endl;
+    } 
+ 
+    // for(int i=1;i<=tc;i++)
+    // {
+    //   cout<<"Case #"<<i<<": ";
+    //   solve();
+    // }
+ 
+    return 0;
+}
